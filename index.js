1bae6c3be2d1cd3afa528c503cda0a0d7c75c4e6
Diego Jofre <diego-jofre@live.com.ar>
2024-04-13 11:33:22 -0300

    ruta para users

--- index.js
+++ index.js
@@ -8,23 +8,24 @@ var cors = require('cors')
 
 // initialise express
 const app = express();
+app.use(express.json())
 app.use(cors())
 
 //  mondodb connect
 mongoose
-.connect(
-    process.env.MONGODB_URI, 
-    {useNewUrlParser: true, useUnifiedTopology: true}
-)
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+    .connect(
+        process.env.MONGODB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
 
 // create a schema
 
-const materialSchema = new mongoose.Schema({ 
-    nivel:String,
+const materialSchema = new mongoose.Schema({
+    nivel: String,
     palabrasClave: String,
-    privilegios: Boolean ,
+    privilegios: Boolean,
     tipo: String,
     titulo: String,
     urlTitulo: String,
@@ -37,33 +38,58 @@ const materialSchema = new mongoose.Schema({
 const Material = mongoose.model('Material', materialSchema);
 
 const reviewSchema = new mongoose.Schema({
-  author: String,
-  pictureUrl: String,
-  position: String,
+    author: String,
+    pictureUrl: String,
+    position: String,
 });
 
 const Review = mongoose.model('Review', reviewSchema);
 
+// create a model with userShema
+
+const userSchema = new mongoose.Schema({
+    email: String
+})
+const User = mongoose.model("User", userSchema);
+
+
 // get documents
 app.get('/api/materials', (req, res) => {
     Material.find()
-    .then((result) => {
-        res.send(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 app.get('/api/reviews', (req, res) => {
-  Review.find()
-  .then((result) => {
-      res.send(result);
-  })
-  .catch((err) => {
-      console.log(err);
-  });
+    Review.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
+
+// post useremai
+
+app.post('/api/users', (req, res) => {
+    const email = req.body.email
+    const user = User.findOne({ email });
+    if (user) res.send("already exists")
+    User.create({ email })
+        .then((result) => {
+            res.send("user " + result.email + " created");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
 
 // Server listen
 app.listen(3001, () => console.log("Server listening to port 3001"));
